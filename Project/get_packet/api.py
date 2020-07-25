0273e02f72340da3cdd353866f4b9035bb6ae1f4
yubowen <88382078@qq.com>
2020-07-25 22:04:38 +0800

    ips web server

--- Project/get_packet/api.py
+++ Project/get_packet/api.py
@@ -3,21 +3,22 @@ from flask_restful import reqparse
 
 
 class Statu():
-    def __init__(self,statu):
+    def __init__(self, statu):
         self.statu = statu
+
 
 # TodoList
 # shows a list of all todos , and lets you POST to add new tasks
 class getPacket(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('config',type=dict,required=True,help="Name cannot be blank!")
-    parser.add_argument('sec',type=int,required=True,help="Name cannot be blank!")
+    parser.add_argument('config', type=dict, required=True, help="Name cannot be blank!")
+    parser.add_argument('sec', type=int, required=True, help="Name cannot be blank!")
     parser.add_argument('usec', type=int, required=True, help="Name cannot be blank!")
     parser.add_argument('ip_offset', type=int, required=True, help="Name cannot be blank!")
-    parser.add_argument('packet', type=str, required=True, help="Name cannot be blank!")
+    parser.add_argument('packet', type=bytes, required=True, help="Name cannot be blank!")
 
     resource_fields = {
-        'statu':fields.Integer
+        'statu': fields.Integer
     }
 
     @marshal_with(resource_fields, envelope='resource')
@@ -30,17 +31,13 @@ class getPacket(Resource):
             usec = args['usec']
             ip_offset = args['ip_offset']
             print('---------------------------------------------------------------------------')
-            print("sec:%d, usec:%d, ip_offset:%d, get packet: %s" % (sec,usec,ip_offset,packet))
+            print("sec:%d, usec:%d, ip_offset:%d" % (sec, usec, ip_offset))
+            print("get packet: ", packet)
             print('----------------------------------------------------------------------------')
             return Statu(1), 200
         else:
             return Statu(-1), 200
 
-
-
-
     @marshal_with(resource_fields, envelope='resource')
     def get(self):
         return "hello"
-
-
